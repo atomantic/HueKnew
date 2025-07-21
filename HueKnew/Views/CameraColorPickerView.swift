c614d2663ec1af3d718771397b59b03647d63366
Adam Eivy <atomantic@gmail.com>
2025-07-21 12:26:05 -0700

    and .up

--- HueKnew/Views/CameraColorPickerView.swift
+++ HueKnew/Views/CameraColorPickerView.swift
@@ -288,7 +288,7 @@ class CameraController: UIViewController, AVCaptureVideoDataOutputSampleBufferDe
         guard let buffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
         let ciImage = CIImage(cvPixelBuffer: buffer)
         if let cgImage = context.createCGImage(ciImage, from: ciImage.extent) {
-            let frameImage = UIImage(cgImage: cgImage, scale: 1.0, orientation: .right)
+            let frameImage = UIImage(cgImage: cgImage, scale: 1.0, orientation: .up)
             delegate?.didOutput(image: frameImage)
         }
     }
